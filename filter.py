09fc4822dbf9ab467d4b5f114a28553002f8716d
安久琲奈/LunaticGhoulPiano <38567204+LunaticGhoulPiano@users.noreply.github.com>
2025-04-13 05:21:41 +0800

    Finish flitering readsb-hist, TODO: reduce traces

--- filter.py
+++ filter.py
@@ -1,6 +1,9 @@
 import os
 import json
 import folium
+import pandas as pd
+from tqdm import tqdm
+from datetime import datetime
 from shapely.geometry import Point, Polygon
 
 # transform DMS(Degrees, Minutes, Seconds) to DD(Decimal Degrees)
@@ -41,12 +44,165 @@ def in_region(lon:float, lat:float, polygon:Polygon):
     point = Point(lon, lat)
     return polygon.contains(point)
 
+def filter_readsb_hist(region:str):
+    # check output path
+    data_path = "./data./historical_adsbex_sample./readsb-hist"
+    file_path = f"./data./filtered./filtered_by_{region[:-5]}.csv" # ".json" -> ".csv"
+    #if os.path.exists(file_path):
+    #    shutil.rmtree(file_path)
+    os.makedirs("./data./filtered", exist_ok = True)
+    
+    # filter
+    polygon = make_boundary(region)
+    for snapshot_json in os.listdir(data_path):
+        # file of a specific time
+        json_file = json.load(open(f"{data_path}./{snapshot_json}", "r", encoding = "utf-8"))
+        
+        # features
+        year, month, date, time = snapshot_json[:-5].split("_")
+        hour, minute, second = time[:2], time[2:4], time[4:] # 000000: hhmmss
+        time_headers = ["year", "month", "date", "hour", "minute", "second"]
+        feature_headers = ["hex", "flight", "t", "alt_baro", "alt_geom", "gs", "track", "geom_rate", "squawk", \
+                   "nav_qnh", "nav_altitude_mcp", "nav_altitude_fms", "nav_heading", \
+                    "lat", "lon", "nic", "rc", "track", "nic_baro", "nac_p", "nac_v", "sil", "sil_type"]
+        full_headers = time_headers + feature_headers
+        
+        # make dataframe
+        df = pd.DataFrame(columns = time_headers + feature_headers)
+        
+        # filter
+        i = 0
+        for aircraft in json_file["aircraft"]:
+            if all(feature in aircraft.keys() for feature in feature_headers) and in_region(aircraft["lon"], aircraft["lat"], polygon):
+                # add to dataframe
+                new_row = pd.DataFrame([[year, month, date, hour, minute, second] + [aircraft[feature].strip() for feature in feature_headers]], columns = full_headers)
+                df = pd.concat([df, new_row], ignore_index = True)
+    # save
+    df.to_csv(file_path, index = False)
+    
+    # log
+    with open(f"./logs./readsb-hist_filtered_by_{region[:-5]}.txt", "a", encoding = "utf-8") as f:
+        f.write(f"> Update at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        f.write(f"Total number of aircrafts: {df.shape[0]}\n\n")
+
+def reduce_traces(hires:bool = False):
+    res = "hires-traces" if hires else "traces"
+    # check output path
+    data_path = f"./data./historical_adsbex_sample./{res}"
+    file_path = f"./data./filtered./{res}.csv" # ".json" -> ".csv"
+    #if os.path.exists(file_path):
+    #    shutil.rmtree(file_path)
+    os.makedirs("./data./filtered", exist_ok = True)
+    """
+    # calculate threshold of the number of traces
+    # the threshold is 15% of the average (manually set)
+    factor = 0.05
+    has_data_counts = []
+    for aircraft in tqdm(os.listdir(data_path), desc = "Calculating threshold", unit = " aircrafts"):
+        jf = json.load(open(f"{data_path}./{aircraft}", "r", encoding = "utf-8"))
+        i = 0
+        for trace in jf["trace"]:
+            if trace[8]:
+                i += 1
+        has_data_counts.append(i)
+    threshold = int(round(round(sum(has_data_counts) / len(has_data_counts), 1) * factor, 1))
+    """
+    threshold = 18
+    
+    # check aircraft
+    k = 0
+    for aircraft in tqdm(os.listdir(data_path), desc = "Reducing traces", unit = " aircrafts"):
+        # file of a specific time
+        json_file = json.load(open(f"{data_path}./{aircraft}", "r", encoding = "utf-8"))
+
+        # first filter: if has key "noRegData" and its value is True
+        if "noRegData" in json_file.keys() and json_file["noRegData"] == True:
+            continue
+        
+        # features
+        year, month, date, _ = aircraft[:-5].split("_")
+        hex = json_file["icao"]
+        timestamp = json_file["timestamp"] # start time: unix timestamp in seconds since epoch (1970)
+        before_features = {}
+        i = 0
+
+        for trace in json_file["trace"]:
+            # index meanings: see "Trace File Fields" at https://www.adsbexchange.com/version-2-api-wip/
+            # 0: seconds after timestamp,
+            # 1: lat,
+            # 2: lon,
+            # 3: altitude in ft or "ground" or null
+            # 4: ground speed in knots or null
+            # 5: track in degrees or null (if altitude == "ground", this will be true heading instead of track)
+            # 6: flags as a bitfield: (use bitwise and to extract data)
+            #    (flags & 1 > 0): position is stale (no position received for 20 seconds before this one)
+            #    (flags & 2 > 0): start of a new leg (tries to detect a separation point between landing and takeoff that separates fligths)
+            #    (flags & 4 > 0): vertical rate is geometric and not barometric
+            #    (flags & 8 > 0): altitude is geometric and not barometric
+            # 7: vertical rate in fpm or null
+            # 8: aircraft object with extra details or null (see aircraft.json documentation,
+            #    note that not all fields are present as lat and lon for example arlready in the values above)
+            # -- the following fields only in files generated 2022 and later: --
+            # 9: type / source of this position or null
+            # 10: geometric altitude or null
+            # 11: geometric vertical rate or null
+            # 12: indicated airspeed or null
+            # 13: roll angle or null
+
+            # second filter: the usable traces
+            print(trace)
+            k += 1
+            if k == 10:
+                exit()
+            continue
+            if not trace[0] or not trace[1] or not trace[2] or not trace[4] or not trace[5] or not trace[6] or not trace[7] or not trace[8]:
+                continue
+            
+            # third filter: if these keys not in trace[8]
+            requirements =  ["type", "alt_geom", "squawk", "baro_rate", "nav_qnh", "nav_altitude_mcp", "nic", "rc", "nic_baro", "nac_p", "nac_v", "gva"]
+            if not all(key in trace[8].keys() for key in requirements):
+                continue
+            
+            # build feature table by time
+            before_features[str(trace[0])] = {
+                "lat": trace[1],
+                "lon": trace[2],
+                "alt_geom": trace[8]["alt_geom"],
+                "gs": trace[4],
+                "track": trace[5],
+                "flags": trace[6],
+                "v_rate": trace[7],
+                "squawk": trace[8]["squawk"],
+                "baro_rate": trace[8]["baro_rate"],
+                "nav_qnh": trace[8]["nav_qnh"],
+                "nav_altitude_mcp": trace[8]["nav_altitude_mcp"],
+                "nic": trace[8]["nic"],
+                "rc": trace[8]["rc"],
+                "nic_baro": trace[8]["nic_baro"],
+                "nac_p": trace[8]["nac_p"],
+                "nac_v": trace[8]["nac_v"],
+                "gva": trace[8]["gva"]
+            }
+        
+        # fourth filter: if the number of trace features is less than threshold
+        print(aircraft, len(before_features))
+        k += 1
+        if k == 10:
+            exit()
+        #if len(before_features) < threshold:
+        #    continue
+
+        # first reduce: get the traces evenly by threshold in before_features
+        index_of_usable_traces = list(before_features.keys())[::threshold]
+        print(index_of_usable_traces)
+
 # test
 if __name__ == "__main__":
     #polygon = make_boundary("Taiwan_ADIZ.json")
-    polygon = make_boundary("Taiwan_manual_edges.json")
-    test_point = Point(120.006663, 22.999459) # 22°59'58.1"N 120°00'24.0"E
-    if polygon.contains(test_point):
-        print("In region")
-    else:
-        print("Not in region")
+    #polygon = make_boundary("Taiwan_manual_edges.json")
+    #test_point = Point(120.006663, 22.999459) # 22°59'58.1"N 120°00'24.0"E
+    #if polygon.contains(test_point):
+    #    print("In region")
+    #else:
+    #    print("Not in region")
+    reduce_traces()
--- preprocessor.py
+++ preprocessor.py
@@ -1,16 +1,11 @@
 import os
-import gzip
-import json
 import filter
 import get_data
-from tqdm import tqdm
 
 def preprocess():
-    # data path
-    original_path = "./data./historical_adsbex_sample./readsb-hist"
-    
     # download data
-    if (not os.path.exists(original_path)) or input("Do you want to download / update data?\nIf no then use current data (y/n): ").lower() == "y":
+    if (not os.path.exists("./data./historical_adsbex_sample")) or \
+        input("Do you want to download / update data?\nIf no then use current data (y/n): ").lower() == "y":
         get_data.get_data()
     
     # let user choose a region file to filter
@@ -27,49 +22,9 @@ def preprocess():
             print("Invalid input, try again.")
         except KeyError:
             print("No such file, try again.")
-
-    # check path
-    new_path = f"./data./filtered./filtered_by_{region[:-5]}" # remove ".json"
-    #if os.path.exists(new_path):
-    #    shutil.rmtree(new_path)
-    #os.makedirs(new_path)
-    os.makedirs(new_path, exist_ok = True)
     
-    # filter
-    polygon = filter.make_boundary(region)
-    for year in os.listdir(original_path):
-        for month in os.listdir(f"{original_path}./{year}"):
-            for date in os.listdir(f"{original_path}./{year}./{month}"):
-                for file_by_time in os.listdir(f"{original_path}./{year}./{month}./{date}"):
-                    if file_by_time.endswith(".json.gz"): # unzip and load as a dict
-                        with gzip.open(f"{original_path}./{year}./{month}./{date}./{file_by_time}", "rb") as f_in:
-                            unzipped_dicts = json.load(f_in)
-                    elif file_by_time.endswith(".json"): # load as a dict
-                        with open(f"{original_path}./{year}./{month}./{date}./{file_by_time}", "r", encoding = "utf-8") as f_in:
-                            unzipped_dicts = json.load(f_in)
-                    else:# ignore
-                        continue
-                    # fetch time
-                    hour = file_by_time[:2]
-                    minute = file_by_time[2:4]
-                    second = file_by_time[4:6]
-                    
-                    # filtered file
-                    file_path = f"{new_path}./{year}_{month}_{date}_{hour}_{minute}_{second}.json"
-                    
-                    # get size
-                    size = len(unzipped_dicts["aircraft"])
-                    filtered = []
-                    
-                    # filter
-                    for i, aircraft in tqdm(enumerate(unzipped_dicts["aircraft"]), total = size, desc = f"Filtering {file_path}"):
-                        if "lat" in aircraft and "lon" in aircraft and filter.in_region(aircraft["lon"], aircraft["lat"], polygon):
-                            filtered.append(aircraft)
-                    
-                    # write only if has data
-                    if filtered:
-                        with open(file_path, "w", encoding = "utf-8") as f_out:
-                            json.dump(filtered, f_out, indent = 4)
+    filter.filter_readsb_hist(region)
+    #filter.reduce_traces() # send True if you use hires-traces
 
 if __name__ == "__main__":
     preprocess()
